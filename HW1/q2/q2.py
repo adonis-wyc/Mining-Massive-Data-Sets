2aa2140de329a54711a408cbf2eef0f3495aa1a1
Austin Jones <austinhj@stanford.edu>
2019-01-21 19:53:27 -0800

    Finished q2, identifying frequent triples and computing confidences of the corresponding rules

--- HW1/q2/q2.py
+++ HW1/q2/q2.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from itertools import combinations
 
-# Counts number of occurences of each individual item, then
+# Counts number of occurrences of each individual item, then
 # prunes items that don't meet threshold support. 
 def get_singles(infile, singles, support):
     with open(infile) as browsing_history:
@@ -14,7 +14,7 @@ def get_singles(infile, singles, support):
             del singles[k] 
 
 
-# Counts number of occurences of each tuple made up of frequent singles,
+# Counts number of occurrences of each tuple made up of frequent singles,
 # then prunes tuples that don't meet threshold support.
 def get_tuples(infile, singles, tuples, support):
     with open(infile) as browsing_history:
@@ -28,19 +28,44 @@ def get_tuples(infile, singles, tuples, support):
             del tuples[k]
 
 
-# Given a set of frequent tuples, computes the confidence scores of the tuple:
+# Given a set of frequent tuples, computes the confidence scores of each tuple:
 # conf(X -> Y) and conf (Y -> X), where conf(X -> Y) = support(X intersect Y) / support(X) 
 def compute_tuple_confidences(singles, tuples, tuple_confidences):
     for pair in tuples:
         tuple_confidences[pair] = float(tuples[pair]) / singles[pair[0]]
         tuple_confidences[(pair[1], pair[0])] = float(tuples[pair]) / singles[pair[1]]
         
+
+# Counts the number of occurrences of each triple made up of frequent tuples,
+# then prunes triples that don't meet threshold support.
+def get_triples(infile, tuples, triples, support):
+    with open(infile) as browsing_history:
+        for line in browsing_history:
+            for triple in combinations(sorted(line.split()), 3):
+                if ((triple[0], triple[1]) in tuples and (triple[0], triple[2]) in tuples and 
+                    (triple[1], triple[2]) in tuples):
+                    triples[triple] += 1
+        browsing_history.close()
+    for k, v in triples.items():
+        if v < support:
+            del triples[k]
+
+
+# Given a set of frequent triples, computes the confidence scores of each triple:
+# conf((X, Y) -> Z), conf((X, Z) -> Y), and conf((Y, Z) -> X), where conf((X, Y) -> Z) = 
+# support((X, Y) intersect Z) / support((X, Y))
+def compute_triple_confidences(tuples, triples, triple_confidences):
+    for triple in triples:
+        triple_confidences[triple] = float(triples[triple]) / tuples[(triple[0], triple[1])]
+        triple_confidences[(triple[0], triple[2], triple[1])] = float(triples[triple]) / tuples[(triple[0], triple[2])]
+        triple_confidences[(triple[1], triple[2], triple[0])] = float(triples[triple]) / tuples[(triple[1], triple[2])]
+
     
 # Main function. 
-# 1) Count occurences of individual items, then remove those that don't meet support.
-# 2) Count occurences of tuples where both elements are frequent items, then remove 
+# 1) Count occurrences of individual items, then remove those that don't meet support.
+# 2) Count occurrences of tuples where both elements are frequent items, then remove 
 #    those that don't meet support.
-# 3) Count occurences of triples where all elemenents are frequent items, and the 3 
+# 3) Count occurrences of triples where all elemenents are frequent items, and the 3 
 #    possible pairs also are frequent, then remove those that don't meet support. 
 def solution(infile, support):
     singles = defaultdict(int) 
@@ -51,10 +76,14 @@ def solution(infile, support):
     get_singles(infile, singles, support)         # singles now contains only frequent items
     get_tuples(infile, singles, tuples, support)  # tuples now contains only frequent pairs
     compute_tuple_confidences(singles, tuples, tuple_confidences)
-    result = sorted(tuple_confidences, key=tuple_confidences.get, reverse=True)
-    for i in range(5):
-        print result[i], tuple_confidences[result[i]]
-    
+    top_tuple_confidences = sorted(tuple_confidences, key=tuple_confidences.get, reverse=True)[0:5]
+    # for pair in top_tuple_confidences:
+        # print pair, tuple_confidences[pair]
+    get_triples(infile, tuples, triples, support) # triples now contains only frequent triples
+    compute_triple_confidences(tuples, triples, triple_confidences)
+    top_triple_confidences = sorted(triple_confidences, key=triple_confidences.get, reverse=True)[0:50]
+    # for triple in top_triple_confidences:
+        # print triple, triple_confidences[triple]
 
 
 solution('data/browsing.txt', 100)
